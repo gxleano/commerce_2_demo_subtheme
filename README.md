2f4139d150d7e49968a3b9335f0c3fe1c57e5f96
gxleano <galeanobarradodavid@gmail.com>
2020-03-27 13:17:27 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# commerce_2_demo_subtheme
-Drupal commerce subtheme by Acromedia Orange Framework
+<!-- @file Project Page -->
+# Orange Starter
+
+> Custom theme by Acro Media Inc.
+
+This custom theme uses the Orange Framework as a base theme.
+
+### SASS
+- Configured to compile using Gulp.
+- Compile to CSS by running the following commands:
+  - `npm install`
+  - `gulp`
+- The CSS will be compiled to: `css/style.css`
---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var sourcemaps = require('gulp-sourcemaps');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var watch = require('gulp-watch');
+
+// Compile the CSS and other tricks from the dark arts.
+gulp.task('sass', function () {
+  "use strict";
+  return gulp.src('./sass/custom/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+      errLogToConsole: true
+    }))
+    .pipe(postcss([
+      autoprefixer({
+        browsers: ['> 5%', 'safari 8', 'firefox 60']
+      })
+    ]))
+    .pipe(sourcemaps.write())
+    .pipe(rename('style.css'))
+    .pipe(gulp.dest('./css'));
+});
+
+// Watch for Sass file changes.
+gulp.task('watch', function () {
+  "use strict";
+  return gulp.watch('./sass/custom/*.scss', gulp.series('sass'))
+});
+
+// Default task.
+// Ensure the CSS is compiled and then watch for file changes.
+gulp.task('default', gulp.series('sass', 'watch'));
